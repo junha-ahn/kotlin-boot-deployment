8653411c74f546de0f4bb4cb2a02a264ec1ee075
Junha <junha.ahn.dev@gmail.com>
2023-06-25 06:40:57 +0000

    test: add example test

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -2,12 +2,21 @@ package com.example.demo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.Assertions.assertEquals
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun testIncrement() {
+        // Given
+        val number = 5
+
+        // When
+        val result = number + 1
+
+        // Then
+        assertEquals(6, result)
+    }
 
 }
