1fb9b5398e08c34bf405d7778ee08f6be7463079
Junha <junha.ahn.dev@gmail.com>
2023-07-08 09:50:36 +0000

    chore: add security gruop (aws)

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "master" {
     ami           = "ami-0c9c942bd7bf113a2"
     instance_type = "t2.micro"
+    subnet_id = aws_subnet.public_subnet.id
+    vpc_security_group_ids = [aws_security_group.cicd_sg.id]
     tags = {
         Name = "master"
     }
@@ -9,6 +11,8 @@ resource "aws_instance" "master" {
 resource "aws_instance" "worker1" {
     ami           = "ami-0c9c942bd7bf113a2"
     instance_type = "t2.micro"
+    subnet_id = aws_subnet.public_subnet.id
+    vpc_security_group_ids = [aws_security_group.cicd_sg.id]
     tags = {
         Name = "worker1"
     }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "kotlin_app_vpc" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.kotlin_app_vpc.id
   cidr_block = "10.0.1.0/24"
 
   availability_zone = "ap-northeast-2a"
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.kotlin_app_vpc.id
 
   tags = {
     Name = "app-igw"
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "igw" {
 
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.kotlin_app_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
