b5f9b680646176685d5fec72c6f077140a6e0961
Junha <junha.ahn.dev@gmail.com>
2023-07-08 14:02:28 +0000

    chore: sg outbound

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "master" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnet.id
     vpc_security_group_ids = [aws_security_group.cicd_sg.id]
+    associate_public_ip_address = true
     tags = {
         Name = "master"
     }
--- terraform/security.tf
+++ terraform/security.tf
@@ -30,6 +30,26 @@ resource "aws_security_group_rule" "cicd_sg_ingress_http" {
   security_group_id = aws_security_group.cicd_sg.id
 }
 
+
+resource "aws_security_group_rule" "cicd_sg_egress_https" {
+  type        = "egress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cicd_sg.id
+}
+
+resource "aws_security_group_rule" "cicd_sg_egress_http" {
+  type        = "egress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cicd_sg.id
+}
+
+
 # Allow all traffic from the same security group
 # resource "aws_security_group_rule" "cicd_sg_ingress_internal" {
 #   type               = "ingress"
