e71ced30e9f176d036bca83c367e3429cb9c8625
Junha <junha.ahn.dev@gmail.com>
2023-07-09 01:57:31 +0000

    chore: add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
+    id("jacoco")
 }
 
 group = "com.example"
@@ -13,6 +14,62 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
+}
+
+subprojects {
+    apply(plugin = "jacoco")
+
+    jacoco {
+        toolVersion = "0.8.10"
+    }
+}
+
+tasks.jacocoTestReport {
+
+    dependsOn(tasks.test)
+    reports {
+        html.isEnabled = true
+        csv.isEnabled = false
+        xml.isEnabled = true
+    }
+
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+
+    var Qdomains = mutableListOf<String>()
+
+    for (qPattern in 'A'..'Z') {
+        Qdomains.add("*.Q$qPattern*")
+    }
+
+    violationRules {
+        rule {
+            element = "CLASS"
+
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                // minimum = "0.5".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                // maximum = "0.5".toBigDecimal()
+            }
+
+            // 빈 줄을 제외한 코드의 라인수를 최대 200라인으로 제한합니다.
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "200".toBigDecimal()
+            }
+
+            excludes = Qdomains
+        }
+    }
 }
 
 repositories {
@@ -35,6 +92,7 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 val installLocalGitHook = tasks.register<Copy>("installLocalGitHook") {
